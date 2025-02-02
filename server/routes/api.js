5397f5dfd948ba0e2c39a5767492740c45931abd
steyeung <stevenyeung1020@gmail.com>
2025-02-02 15:25:38 -0500

    feature (server): built endpoints for user and userPerk tables

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import choresRouter from './chores.js';
 import userRouter from './user.js';
+import userPerksRouter from './userPerks.js';
+import perksRouter from './perks.js';
 
 const apiRouter = express.Router();
 
 apiRouter.use('/chores', choresRouter);
 apiRouter.use('/users', userRouter);
+apiRouter.use('/userPerks', userPerksRouter);
+apiRouter.use('/perks', perksRouter);
 
 export default apiRouter;
