83c76b3af0df47d9b9570f5f2f11f3c73f4eafde
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-02-02 18:54:09 -0600

    feature (PerksList) created Input and Row components; added token and quantity input fields; added Perk type definitions

--- src/types.ts
+++ src/types.ts
@@ -8,10 +8,38 @@ interface Chore {
     created_at: string;
   }
 
+  
   interface ChoreSubmitData {
-    user_id: number;
-    task_name: string;
-    tokens: number;
-  }
+      user_id: number;
+      task_name: string;
+      tokens: number;
+    }
+    
+    interface Perk {
+      id: number;
+      perk_name: string;
+      tokens: number;
+      qty_initial: number;
+      qty_remaining: number
+    }
 
-  export type { Chore, ChoreSubmitData };
+    interface PerkSubmitData {
+        perk_name: string;
+        tokens: number;
+        qty: number;
+
+    }
+
+  export type { Chore, ChoreSubmitData, Perk, PerkSubmitData };
+
+  /* CHORE
+{
+    "id": 10,
+    "task_name": "Mow lawn",
+    "tokens": 10,
+    "user_id": 2,
+    "is_complete": false,
+    "due_date": "2025-02-10T06:00:00.000Z",
+    "created_at": "2025-01-28T06:00:00.000Z"
+}
+*/
