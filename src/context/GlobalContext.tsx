bf649a403845825999e56503efe90be94a6e6df6
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-02-04 00:17:34 -0600

    refactor (Context) refactored global state object and reducer cases

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -2,6 +2,7 @@
 import React, { createContext, useReducer, useEffect } from "react";
 import * as types from "../types";
 // import custom hooks
+import { getUser } from "../actions/userActions";
 import { getChores } from "../actions/choreActions";
 import { getPerks } from "../actions/perkActions";
 // define dispatch ACTION TYPES and type definitions
@@ -29,6 +30,7 @@ returns updated state;
  * ACTION TYPES
  */
 interface GlobalContextActionTypes {
+  GET_USER: string;
   GET_CHORES: string;
   CREATE_CHORE: string;
   COMPLETE_CHORE: string;
@@ -39,6 +41,7 @@ interface GlobalContextActionTypes {
 }
 
 const ActionTypes: GlobalContextActionTypes = {
+  GET_USER: "GET_USER",
   GET_CHORES: "GET_CHORES",
   CREATE_CHORE: "CREATE_CHORE",
   COMPLETE_CHORE: "COMPLETE_CHORE",
@@ -52,17 +55,26 @@ const ActionTypes: GlobalContextActionTypes = {
  * APPLICATION STATE
  */
 interface GlobalState {
-  user_id: number;
+  userInfo: types.User;
+  userInventory: { userPerks: types.UserPerk[]; choreHistory: types.Chore[] };
   chores: types.Chore[];
   perks: types.Perk[];
-  userPerks: any[];
 }
 
 const initialState: GlobalState = {
-  user_id: 1,
+  userInfo: {
+    id: 0,
+    username: "",
+    email: "",
+    tokens: 0,
+    created_at: "",
+  },
+  userInventory: {
+    userPerks: [],
+    choreHistory: [],
+  },
   chores: [],
   perks: [],
-  userPerks: [],
 };
 
 /*
@@ -75,25 +87,46 @@ interface DispatchAction {
 
 const reducer = (state: GlobalState, action: DispatchAction) => {
   switch (action.type) {
+    case ActionTypes.GET_USER:
+      //   console.log("FETCHED USER:", action.payload);
+      const user = action.payload;
+      return { ...state, userInfo: user };
+
     case ActionTypes.GET_CHORES:
-      console.log("CHORES", action.payload.chores);
-      const fetchedChores = action.payload.chores;
+      //   console.log("FETCHED CHORES", action.payload.chores);
+      const fetchedChores = action.payload;
       return { ...state, chores: fetchedChores };
 
     case ActionTypes.CREATE_CHORE:
-      console.log("NEW CHORE:", action.payload);
+      console.log("CREATED CHORE:", action.payload);
       const newChore = action.payload;
       return { ...state, chores: [...state.chores, newChore] };
 
+    case ActionTypes.COMPLETE_CHORE:
+      console.log("COMPLETED CHORE:", action.payload);
+      const completedChore = action.payload;
+      const updatedChoreHistory = {
+        ...state.userInventory,
+        choreHistory: [...state.userInventory.choreHistory, completedChore],
+      };
+      return {
+        ...state,
+        userInventory: updatedChoreHistory,
+      };
+
     case ActionTypes.GET_PERKS:
-      console.log("PERKS:", action.payload.perks);
-      const fetchedPerks = action.payload.perks;
+      //   console.log("FETCHED PERKS:", action.payload.perks);
+      const fetchedPerks = action.payload;
       return { ...state, perks: fetchedPerks };
 
     case ActionTypes.CREATE_PERK:
-      console.log("NEW PERK:", action.payload);
-      //! server response is an array. can it be single object?
+      console.log("CREATED PERK:", action.payload);
       const [newPerk] = action.payload;
+      return { ...state, perks: [...state.perks, newPerk] };
+
+    case ActionTypes.PURCHASE_PERK:
+      console.log("PURCHASED PERK:", action.payload);
+      const [purchasedPerk] = action.payload;
       return { ...state, perks: [...state.perks, newPerk] };
 
     default:
@@ -121,6 +154,7 @@ const GlobalProvider: React.FC<{ children: React.ReactNode }> = ({
 
   // populate state on initial render
   useEffect(() => {
+    getUser()(dispatch);
     getChores()(dispatch);
     getPerks()(dispatch);
   }, [dispatch]);
