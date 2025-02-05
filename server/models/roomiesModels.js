b78168f4c73481caa6b94d50173d3fcee947f5bd
steyeung <stevenyeung1020@gmail.com>
2025-02-04 20:36:38 -0500

    testing PG_URI

--- server/models/roomiesModels.js
+++ server/models/roomiesModels.js
@@ -26,6 +26,7 @@ const queryConnection = async () => {
   try {
     const res = await pool.query('SELECT NOW()');
     console.log('DATABASE SUCCESSFULLY CONNECTED: ', res.rows[0]);
+    console.log("Database URL:", process.env.PG_URI);
   } catch (err) {
     console.error('ERROR IN CONNECTING TO DATABASE');
   }
