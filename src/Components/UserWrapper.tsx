06b60ee1ba835c97b523e146f8cad88bca1f21cb
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-02-05 12:34:56 -0600

    style (UserWrapper) added flex styling to user wrapper

--- src/Components/UserWrapper.tsx
+++ src/Components/UserWrapper.tsx
@@ -10,12 +10,14 @@ const UserWrapper = () => {
   const { userPerks, choreHistory } = state.userInventory;
 
   return (
-    <div className="p-2 m-4 h-8/10 w-1/2 border-white rounded-[50px] border-5">
+    <div className="p-2 m-4 h-8/10 border-white rounded-[50px] border-5">
       <h1 className="text-2xl font-display font-semibold text-sky-900">
         #USER WRAPPER#
       </h1>
-      <UserPerks perkData={userPerks} />
-      <CompletedChores choreData={choreHistory} />
+      <div className="flex">
+          <UserPerks perkData={userPerks} />
+          <CompletedChores choreData={choreHistory} />
+      </div>
     </div>
   );
 };
