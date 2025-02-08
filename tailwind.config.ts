357131542b6718aed1730b99bcb61a4ce0915633
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-02-07 18:40:14 -0600

    styling changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,11 @@ const config: Config = {
     './src/Components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {}, 
+    extend: {
+      boxShadow: {
+        'custom-shadow': '0 10px 25px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.1), 0 20px 25px -5px rgba(0, 0, 0, 0.1), inset 0 2px 2px rgba(255, 255, 255, 0.95)',
+      },
+    }, 
   },
   plugins: [], 
   darkMode: 'class',
